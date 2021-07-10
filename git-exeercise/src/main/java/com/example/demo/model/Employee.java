a4da34a6d9386c422ae47d0a7029688ff18fa01a
srivatsan <vatsank@gmail.com>
2021-07-10 21:49:37 +0530

    Employee Java class added

---
+++ git-exeercise/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+public class Employee {
+
+	int id;
+	String firstName;
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+}
