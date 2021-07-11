5b312f8383ab574100f02880fe0aef842aa17a3b
srivatsan <vatsank@gmail.com>
2021-07-11 14:15:50 +0530

    Update

--- restaurant-info-service/src/main/java/com/example/demo/handlers/GlobalExceptionHandler.java
+++ restaurant-info-service/src/main/java/com/example/demo/handlers/GlobalExceptionHandler.java
@@ -1,14 +1,28 @@
 package com.example.demo.handlers;
 
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-import com.example.demo.model.ErrrorHandler;
+import com.example.demo.model.ErrorHandler;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	
-	public ErrrorHandler  handleAllExceptions(Exception e, WebRequest request) {
+	@ExceptionHandler(Exception.class)
+	public ErrorHandler  handleAllExceptions(Exception e, WebRequest request) {
 	
-		return null;
+		return new ErrorHandler(LocalDateTime.now(), e.getMessage(),request.getDescription(false));
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public ErrorHandler  handleNoElement(Exception e, WebRequest request) {
+		
+		return  new ErrorHandler(LocalDateTime.now(), "Err-10393-Element Not Found",request.getDescription(false));
+
 	}
 }
